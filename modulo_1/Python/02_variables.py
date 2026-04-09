a624affbddd87878c98a8ad39a13b1048e61854f
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-08 19:55:02 -0500

    Clase 04/08/2026

---
+++ modulo_1/Python/02_variables.py
@@ -0,0 +1,28 @@
+from types import NoneType
+
+
+MAX_INTENTOS = 3
+nombre ="MARCELO BACON"
+edad = 26
+altura= 1.65
+activo = True
+nulo= None
+
+print(nombre, "tipo ", type(nombre))
+print(edad, "tipo ", type(edad))
+print(altura, "tipo ", type(altura))
+print(activo, "tipo ", type(activo))
+print(None, "tipo ", type(None))
+
+
+nombre_apellido : str="BACON MARCELO"
+edad_trabajador : int=28
+altura_trabajador: float=1.65
+activo_trabajador : bool=True
+nulo_trabajador: NoneType=None
+
+print(nombre_apellido, "tipo ", type(nombre_apellido))
+print(edad_trabajador, "tipo ", type(edad_trabajador))
+print(altura_trabajador, "tipo ", type(altura_trabajador))
+print(activo_trabajador, "tipo ", type(activo_trabajador))
+print(nulo_trabajador, "tipo ", type(nulo_trabajador))
